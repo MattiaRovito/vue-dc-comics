ff1fd3db67a662cbd2268759feeac6cf2cd190ad
MattiaR <rovitomattia91@gmail.com>
2021-07-06 17:15:00 +0200

    add header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,6 +3,12 @@
   <div class="container">
       
         <img src="../assets/dc-logo.png" alt="Logo DC">
+
+        <nav>
+          <ul>
+            <li class="uppercase" v-for="(voice, index) in voices" :key="index" :class="{active:voice.current}"><a :href="voice.url">{{ voice.text }} </a></li>
+          </ul>
+        </nav>
       
   </div>
 
@@ -80,10 +86,47 @@ export default {
 <style lang="scss" scoped>
 
 
+.container
+{
+    display: flex;
+}
+
+
 img
 {
-  width: 80px;
+    width: 80px;
+  
 }
+
+ul
+{
+    margin-left: 70px;
+    margin-top: 18px;
+}
+
+li
+{
+    display: inline-block;
+    padding: 10px;
+  
+    a
+    {
+        font-size: 14px;  
+        
+        &:hover
+        {
+            color: blue;
+        }
+    }
+
+    &.active 
+    {
+        border-bottom: 2px solid blue;
+        // padding: 20px;
+    }
+}
+
+
 
 
 
