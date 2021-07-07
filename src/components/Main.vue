1de27dd0905b5989a1e4cd55827c9e37564223b8
MattiaR <rovitomattia91@gmail.com>
2021-07-07 14:11:25 +0200

    add file js Items per Main.vue

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -30,33 +30,13 @@
 </template>
 
 <script>
+import Items from '@/data/Items.js'
 export default {
   name: 'Main',
   data() {
     return {
             
-            items: [
-                {
-                    text: 'Digital Comics',
-                    img:  require('../assets/buy-comics-digital-comics.png')             
-                },
-                {
-                    text: 'DC Merchandise',
-                    img: require('../assets/buy-comics-merchandise.png')            
-                },
-                {
-                    text: 'Subscription',
-                    img: require('../assets/buy-comics-subscriptions.png')                   
-                },
-                {
-                    text: 'Comic Shop Locator',
-                    img: require('../assets/buy-comics-shop-locator.png')                   
-                },
-                {
-                    text: 'DC Power Visa',
-                    img: require('../assets/buy-dc-power-visa.png')                    
-                }
-            ]
+            items: Items
         }
   }
 }
---
+++ src/data/Items.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        text: 'Digital Comics',
+        img:  require('../assets/buy-comics-digital-comics.png')             
+    },
+    {
+        text: 'DC Merchandise',
+        img: require('../assets/buy-comics-merchandise.png')            
+    },
+    {
+        text: 'Subscription',
+        img: require('../assets/buy-comics-subscriptions.png')                   
+    },
+    {
+        text: 'Comic Shop Locator',
+        img: require('../assets/buy-comics-shop-locator.png')                   
+    },
+    {
+        text: 'DC Power Visa',
+        img: require('../assets/buy-dc-power-visa.png')                    
+    }
+]
