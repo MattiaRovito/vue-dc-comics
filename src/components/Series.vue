af30d06aaf419c15a7dfdaa1dd3d255078c722e3
MattiaR <rovitomattia91@gmail.com>
2021-07-07 16:03:11 +0200

    creazione components Series

---
+++ src/components/Series.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="current-series">
+      <img :src="prod.thumb" :alt="prod.series">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Series',
+    props: ["prod"]
+}
+</script>
+
+<style>
+
+
+
+
+</style>
